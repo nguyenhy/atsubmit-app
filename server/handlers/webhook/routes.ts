326fd2e027b1e08bcf11cfbd3a8e1d94495f6819
nguyenhy <nguyenhy311@gmail.com>
2026-03-24 15:52:06 +0700

    fix(webhook): allow post,get method

--- server/handlers/webhook/routes.ts
+++ server/handlers/webhook/routes.ts
@@ -2,7 +2,8 @@ import { WebhookHono } from "@server/types";
 
 export const registerWebhookRoutes = (webhook: WebhookHono) => {
     const aws = webhook.basePath("/aws");
-    aws.get(
+    aws.on(
+        ["post", "get"],
         "/ses/0ba971ed74210dad183e1d71d63c33",
         async (c) => {
             console.log(c.get("reqId"), await c.req.text());
