ed8c336dedd72b49be88657a011070505a161fb3
nguyenhy <nguyenhy311@gmail.com>
2026-03-24 15:43:44 +0700

    fix(webhook): prevent webhook scanning

--- server/handlers/webhook/routes.ts
+++ server/handlers/webhook/routes.ts
@@ -2,8 +2,11 @@ import { WebhookHono } from "@server/types";
 
 export const registerWebhookRoutes = (webhook: WebhookHono) => {
     const aws = webhook.basePath("/aws");
-    aws.get("ses", async (c) => {
-        console.log(c.get("reqId"), await c.req.text());
-        return c.json(null, 200);
-    });
+    aws.get(
+        "/ses/0ba971ed74210dad183e1d71d63c33",
+        async (c) => {
+            console.log(c.get("reqId"), await c.req.text());
+            return c.json(null, 200);
+        },
+    );
 };
