2f8e5d302107eb69e737015b29c265b4d20ab379
nguyenhy <nguyenhy311@gmail.com>
2026-04-03 10:45:17 +0700

    fix: correct mail worker path to handle send email

--- server/modules/SendEmail/SendEmailSignupVerificationService.ts
+++ server/modules/SendEmail/SendEmailSignupVerificationService.ts
@@ -5,7 +5,7 @@ export const sendEmailSignupVerification = async (
     c: MainContext,
     data: { link: string; send_to: string },
 ) => {
-    return sendEmail(c, "/mail/send/ses-bounce", {
+    return sendEmail(c, "/mail/send/signup-verify", {
         link: data.link,
         send_to: data.send_to,
     });
